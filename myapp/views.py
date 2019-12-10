f4b5ff7fecbc337eb9c9710008b31af6e25cfc9c
Arthur Binda <artbinda@gmail.com>
2019-12-09 22:39:38 -0400

    Add: Todas as funções adicionadas

--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from.forms import TrabalhoAlgebraForm, ResultadoForm
+from .forms import TrabalhoAlgebraForm, ResultadoForm
 from .models import Resultado
 import numpy
 from numpy import *
-from numpy.linalg import inv
+
 
 # Create your views here.
 def homepage(request):
     form = TrabalhoAlgebraForm()
     return render(request, 'form.html', {'form': form})
+
 
 def create(request):
     print(request)
@@ -25,7 +26,7 @@ def create(request):
             autovalores = form.cleaned_data['autovalores']
             autovetores = form.cleaned_data['autovetores']
             matriz_diagonal = form.cleaned_data['matriz_diagonal']
-            t = Resultado(id = 1, matriz=matriz, determinante=determinante, traco=traco, transposta=transposta,
+            t = Resultado(id=1, matriz=matriz, determinante=determinante, traco=traco, transposta=transposta,
                           inversa=inversa, polinomio_caracteristico=polinomio_caracteristico, autovalores=autovalores,
                           autovetores=autovetores, matriz_diagonal=matriz_diagonal)
             t.save()
@@ -38,6 +39,7 @@ def create(request):
 
     return render(request, 'form.html', {'form': form})
 
+
 def resultado_detail(request):
     resultado = Resultado.objects.get(id=1)
     resultado = processa_request(resultado)
@@ -45,6 +47,7 @@ def resultado_detail(request):
         "object": resultado
     }
     return render(request, 'resultado.html', context)
+
 
 def processa_request(request):
     matriz = eval(request.matriz)
@@ -61,10 +64,17 @@ def processa_request(request):
         request.transposta = matrizTransposta(matriz)
 
     if request.inversa:
-        request.inversa = inv(matriz)
+        aux = numpy.linalg.inv(matriz)
+        request.inversa = str(aux)
 
     if request.polinomio_caracteristico:
         request.polinomio_caracteristico = polinomioCaracteristico(matriz)
+
+    if request.autovalores:
+        request.autovalores = QR(matriz)
+
+    if request.matriz_diagonal:
+        request.matriz_diagonal = matrizDiagonal(matriz)
 
     return request
 
@@ -76,28 +86,22 @@ def traco(matrix):
     assert n == len(matrix[0])
     return sum(matrix[i][i] for i in range(n))
 
+
 def matrizTransposta(matriz):
-	tam = len(matriz)
-	matrizTransposta = [] # lista vazia para receber a transposta
-	linha = [] # lista auxiliar para receber as linhas
-	for i in range(tam):
-		for j in range(tam):
-			linha.append(matriz[j][i]) # recebe a coluna como linha
-		matrizTransposta.append(linha) # recebe a linha
-		linha = [] # reseta o auxiliar
-		return matrizTransposta
+    tam = len(matriz)
+    matrizTransposta = []  # lista vazia para receber a transposta
+    linha = []  # lista auxiliar para receber as linhas
+    for i in range(tam):
+        for j in range(tam):
+            linha.append(matriz[j][i])  # recebe a coluna como linha
+        matrizTransposta.append(linha)  # recebe a linha
+        linha = []  # reseta o auxiliar
+        return matrizTransposta
 
 
 def somaCoeficientes(matriz, n, q):
     for i in range(n):
         matriz[i][i] += q
-
-
-def traco(matrix, n):
-    sum = 0
-    for i in range(n):
-        sum += matrix[i][i]
-    return sum
 
 
 def polinomioCaracteristico(matriz):
@@ -106,7 +110,53 @@ def polinomioCaracteristico(matriz):
     c = [1.0]
     B = array(A)
     for i in range(1, n + 1):
-        c.append(-traco(B, n) / i)
+        c.append(-traco(B) / i)
         somaCoeficientes(B, n, c[i])
         B = dot(A, B)
     return c
+
+
+def verificaMatrizTriangular(A):
+    for i in range(1, len(A)):
+        for j in range(i):
+            if abs(A[i][j]) > 0.0001:
+                return 1
+    return 0
+
+
+def zeraElementos(A, i, j):
+    U = identity(len(A))
+    U[i][i] = A[0][0] / (((A[0][0] ** 2) + A[i][j] ** 2) ** 0.5)
+    U[j][j] = U[i][i]
+    U[j][i] = A[i][j] / (((A[0][0] ** 2) + A[i][j] ** 2) ** 0.5)
+    U[i][j] = -U[j][i]
+    return U
+
+
+def QR(A):
+    cont = 0
+    tam = len(A)
+    av = []
+    while verificaMatrizTriangular(A) and cont < 1000:
+        Q = identity(tam)
+        for i in range(1, tam):
+            for j in range(i):
+                if A[i][j] != 0:
+                    U = zeraElementos(A, i, j)
+                    Q = dot(U, Q)
+                    A = dot(U, A)
+
+        Q = Q.T
+        A = dot(A, Q)
+        cont += 1
+    for i in range(tam):
+        av.append(A[i][i])
+    return av
+
+def matrizDiagonal(A):
+	n = len(A)
+	matriz = zeros((n,n))
+	autovalores = linalg.eig(A)[0]
+	for i in range(n):
+		matriz[i][i] = autovalores[i]
+	return matriz
